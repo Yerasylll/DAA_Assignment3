4588e9c5fb8fd680a5d12bf61cfd1d18900f6033
Yerasyl Alimbek <erasylalimbek302@gmail.com>
2025-10-26 01:52:27 +0500

    Metrics for performance[C

---
+++ src/main/java/com/company/models/metrics/Metrics.java
@@ -0,0 +1,27 @@
+package com.company.models.metrics;
+
+public class Metrics {
+    private int operationsCount;
+    private long executionTimeMs;
+
+    public Metrics() {
+        this.operationsCount = 0;
+        this.executionTimeMs = 0;
+    }
+
+    public int getOperationsCount() {
+        return operationsCount;
+    }
+
+    public long getExecutionTimeMs() {
+        return executionTimeMs;
+    }
+
+    public void setOperationsCount(int operationsCount) {
+        this.operationsCount = operationsCount;
+    }
+
+    public void setExecutionTimeMs(long executionTimeMs) {
+        this.executionTimeMs = executionTimeMs;
+    }
+}
